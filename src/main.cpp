db832c043f8b1fee133753dba669f431d293533f
Leager-zju <1004729740@qq.com>
2024-04-20 21:39:15 +0800

    assignment4 start

--- src/main.cpp
+++ src/main.cpp
@@ -1,444 +1,80 @@
+#include <chrono>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-#include "global.hpp"
-#include "rasterizer.hpp"
-#include "Triangle.hpp"
-#include "Shader.hpp"
-#include "Texture.hpp"
-#include "OBJ_Loader.h"
+std::vector<cv::Point2f> control_points;
 
-float angleToRadians(float angle) { return MY_PI*angle/180; }
-
-Eigen::Vector3f product(const Eigen::Vector3f& a, const Eigen::Vector3f& b) {
-    return {a.x() * b.x(), a.y() * b.y(), a.z() * b.z()};
-}
-
-Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+void mouse_handler(int event, int x, int y, int flags, void *userdata) 
 {
-    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
-
-    Eigen::Matrix4f translate;
-    translate << 1,0,0,-eye_pos[0],
-                 0,1,0,-eye_pos[1],
-                 0,0,1,-eye_pos[2],
-                 0,0,0,1;
-
-    view = translate*view;
-
-    return view;
-}
-
-Eigen::Matrix4f get_model_matrix(float angle)
-{
-    Eigen::Matrix4f rotation;
-    angle = angle * MY_PI / 180.f;
-    rotation << cos(angle), 0, sin(angle), 0,
-                0, 1, 0, 0,
-                -sin(angle), 0, cos(angle), 0,
-                0, 0, 0, 1;
-
-    Eigen::Matrix4f scale;
-    scale << 2.5, 0, 0, 0,
-              0, 2.5, 0, 0,
-              0, 0, 2.5, 0,
-              0, 0, 0, 1;
-
-    Eigen::Matrix4f translate;
-    translate << 1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1;
-
-    return translate * rotation * scale;
-}
-
-Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
-{
-    // eye_fov: viewing angle in the range of [-eye_fov, eye_fov]
-    // aspect_ratio: the height:width of viewing plane
-    Eigen::Matrix4f squish;
-    Eigen::Matrix4f translation;
-    Eigen::Matrix4f scale;
-
-    float n = -zNear;
-    float f = -zFar;
-
-    squish << n, 0, 0,   0,
-              0, n, 0,   0,
-              0, 0, n+f, -n*f,
-              0, 0, 1,   0;
-    
-    float top = abs(n)*tan(angleToRadians(eye_fov/2));
-    float bottom = -top;
-
-    float right = top*aspect_ratio;
-    float left = -right;
-
-    translation << 1, 0, 0, -(left+right)/2,
-                   0, 1, 0, -(top+bottom)/2,
-                   0, 0, 1, -(n+f)/2,
-                   0, 0, 0, 1;
-
-    scale << 2/(right-left), 0,              0,       0,
-             0,              2/(top-bottom), 0,       0,
-             0,              0,              2/(n-f), 0,
-             0,              0,              0,       1;
-
-    return scale*translation*squish;
-}
-
-Eigen::Vector3f vertex_shader(const vertex_shader_payload& payload)
-{
-    return payload.position;
-}
-
-Eigen::Vector3f normal_fragment_shader(const fragment_shader_payload& payload)
-{
-    Eigen::Vector3f return_color = (payload.normal.head<3>().normalized() + Eigen::Vector3f(1.0f, 1.0f, 1.0f)) / 2.f;
-    Eigen::Vector3f result;
-    result << return_color.x() * 255, return_color.y() * 255, return_color.z() * 255;
-    return result;
-}
-
-static Eigen::Vector3f reflect(const Eigen::Vector3f& vec, const Eigen::Vector3f& axis)
-{
-    auto costheta = vec.dot(axis);
-    return (2 * costheta * axis - vec).normalized();
-}
-
-struct light
-{
-    Eigen::Vector3f position;
-    Eigen::Vector3f intensity;
-};
-
-Eigen::Vector3f texture_fragment_shader(const fragment_shader_payload& payload)
-{
-    Eigen::Vector3f return_color = {0, 0, 0};
-    if (payload.texture)
+    if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4) 
     {
-        float u = payload.tex_coords.x();
-        float v = payload.tex_coords.y();
-        return_color = payload.texture->getColorBilinear(u, v);
+        std::cout << "Left button of the mouse is clicked - position (" << x << ", "
+        << y << ")" << '\n';
+        control_points.emplace_back(x, y);
+    }     
+}
+
+void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
+{
+    auto &p_0 = points[0];
+    auto &p_1 = points[1];
+    auto &p_2 = points[2];
+    auto &p_3 = points[3];
+
+    for (double t = 0.0; t <= 1.0; t += 0.001) 
+    {
+        auto point = std::pow(1 - t, 3) * p_0 + 3 * t * std::pow(1 - t, 2) * p_1 +
+                 3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
+
+        window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
     }
-    Eigen::Vector3f texture_color;
-    texture_color << return_color.x(), return_color.y(), return_color.z();
-
-    Eigen::Vector3f ka = Eigen::Vector3f(0.005, 0.005, 0.005);
-    Eigen::Vector3f kd = texture_color / 255.f;
-    Eigen::Vector3f ks = Eigen::Vector3f(0.7937, 0.7937, 0.7937);
-
-    auto l1 = light{{20, 20, 20}, {500, 500, 500}};
-    auto l2 = light{{-20, 20, 0}, {500, 500, 500}};
-
-    std::vector<light> lights = {l1, l2};
-    Eigen::Vector3f amb_light_intensity{10, 10, 10};
-    Eigen::Vector3f eye_pos{0, 0, 10};
-
-    float p = 150;
-
-    Eigen::Vector3f color = texture_color;
-    Eigen::Vector3f point = payload.view_pos;
-    Eigen::Vector3f normal = payload.normal;
-
-    Eigen::Vector3f result_color = {0, 0, 0};
-
-    for (auto& light : lights)
-    {
-        Vector3f l =
-            (light.position - point).normalized();    // the direction of light
-        Vector3f v = (eye_pos - point).normalized();  // the direction of eyes
-        Vector3f h = (l + v).normalized();  // half vector of light and eyes
-        float r_square = (light.position - point).dot(light.position - point);
-        Vector3f light_intensity = light.intensity / r_square;
-
-        Vector3f ambient_item = product(ka, amb_light_intensity);
-        Vector3f diffuse_item =
-            product(kd, light_intensity) * std::max<float>(0.0, normal.dot(l));
-        Vector3f specular_item =
-            product(ks, light_intensity) *
-            std::pow(std::max<float>(0.0, normal.dot(h)), p);
-        result_color += ambient_item + diffuse_item + specular_item;
-
-    }
-
-    return result_color * 255.f;
 }
 
-Eigen::Vector3f phong_fragment_shader(const fragment_shader_payload& payload)
+cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
 {
-    Eigen::Vector3f ka = Eigen::Vector3f(0.005, 0.005, 0.005);
-    Eigen::Vector3f kd = payload.color;
-    Eigen::Vector3f ks = Eigen::Vector3f(0.7937, 0.7937, 0.7937);
+    // TODO: Implement de Casteljau's algorithm
+    return cv::Point2f();
 
-    auto l1 = light{{20, 20, 20}, {500, 500, 500}};
-    auto l2 = light{{-20, 20, 0}, {500, 500, 500}};
-
-    std::vector<light> lights = {l1, l2};
-    Eigen::Vector3f amb_light_intensity{10, 10, 10};
-    Eigen::Vector3f eye_pos{0, 0, 10};
-
-    float p = 150;
-
-    Eigen::Vector3f color = payload.color;
-    Eigen::Vector3f point = payload.view_pos;
-    Eigen::Vector3f normal = payload.normal;
-
-    Eigen::Vector3f result_color = {0, 0, 0};
-    for (auto& light : lights)
-    {
-        Vector3f l =
-            (light.position - point).normalized();    // the direction of light
-        Vector3f v = (eye_pos - point).normalized();  // the direction of eyes
-        Vector3f h = (l + v).normalized();  // half vector of light and eyes
-        float r_square = (light.position - point).dot(light.position - point);
-        Vector3f light_intensity = light.intensity / r_square;
-
-        Vector3f ambient_item = product(ka, amb_light_intensity);
-        Vector3f diffuse_item =
-            product(kd, light_intensity) * std::max<float>(0.0, normal.dot(l));
-        Vector3f specular_item =
-            product(ks, light_intensity) *
-            std::pow(std::max<float>(0.0, normal.dot(h)), p);
-        result_color += ambient_item + diffuse_item + specular_item;
-    }
-
-    return result_color * 255.f;
 }
 
-
-
-Eigen::Vector3f displacement_fragment_shader(const fragment_shader_payload& payload)
+void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
-    
-    Eigen::Vector3f ka = Eigen::Vector3f(0.005, 0.005, 0.005);
-    Eigen::Vector3f kd = payload.color;
-    Eigen::Vector3f ks = Eigen::Vector3f(0.7937, 0.7937, 0.7937);
+    // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
+    // recursive Bezier algorithm.
 
-    auto l1 = light{{20, 20, 20}, {500, 500, 500}};
-    auto l2 = light{{-20, 20, 0}, {500, 500, 500}};
-
-    std::vector<light> lights = {l1, l2};
-    Eigen::Vector3f amb_light_intensity{10, 10, 10};
-    Eigen::Vector3f eye_pos{0, 0, 10};
-
-    float p = 150;
-
-    Eigen::Vector3f color = payload.color; 
-    Eigen::Vector3f point = payload.view_pos;
-    Eigen::Vector3f normal = payload.normal;
-
-    float kh = 0.2, kn = 0.1;
-    float x = normal.x();
-    float y = normal.y();
-    float z = normal.z();
-    Vector3f t = {x * y / sqrt(x * x + z * z), sqrt(x * x + z * z),
-                  z * y / sqrt(x * x + z * z)};
-    Vector3f b = normal.cross(t);
-    Matrix3f TBN;
-    TBN << t.x(), b.x(), x, t.y(), b.y(), y, t.z(), b.z(), z;
-    float u = payload.tex_coords.x();
-    float v = payload.tex_coords.y();
-    int width = payload.texture->width;
-    int height = payload.texture->height;
-    float dU = kh * kn * (payload.texture->getColor(u + 1.0 / width, v).norm() - payload.texture->getColor(u, v).norm());
-    float dV = kh * kn * (payload.texture->getColor(u, v + 1.0 / height).norm() - payload.texture->getColor(u, v).norm());
-    Vector3f ln = {-dU, -dV, 1};
-    point = point + kn * normal * payload.texture->getColor(u, v).norm();
-    normal = (TBN * ln).normalized();
-
-    Eigen::Vector3f result_color = {0, 0, 0};
-
-    for (auto& light : lights)
-    {
-        Vector3f l =
-            (light.position - point).normalized();    // the direction of light
-        Vector3f v = (eye_pos - point).normalized();  // the direction of eyes
-        Vector3f h = (l + v).normalized();  // half vector of light and eyes
-        float r_square = (light.position - point).dot(light.position - point);
-        Vector3f light_intensity = light.intensity / r_square;
-
-        Vector3f ambient_item = product(ka, amb_light_intensity);
-        Vector3f diffuse_item =
-            product(kd, light_intensity) * std::max<float>(0.0, normal.dot(l));
-        Vector3f specular_item =
-            product(ks, light_intensity) *
-            std::pow(std::max<float>(0.0, normal.dot(h)), p);
-        result_color += ambient_item + diffuse_item + specular_item;
-    }
-
-    return result_color * 255.f;
 }
 
-
-Eigen::Vector3f bump_fragment_shader(const fragment_shader_payload& payload)
+int main() 
 {
-    
-    Eigen::Vector3f ka = Eigen::Vector3f(0.005, 0.005, 0.005);
-    Eigen::Vector3f kd = payload.color;
-    Eigen::Vector3f ks = Eigen::Vector3f(0.7937, 0.7937, 0.7937);
+    cv::Mat window = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
+    cv::cvtColor(window, window, cv::COLOR_BGR2RGB);
+    cv::namedWindow("Bezier Curve", cv::WINDOW_AUTOSIZE);
 
-    auto l1 = light{{20, 20, 20}, {500, 500, 500}};
-    auto l2 = light{{-20, 20, 0}, {500, 500, 500}};
+    cv::setMouseCallback("Bezier Curve", mouse_handler, nullptr);
 
-    std::vector<light> lights = {l1, l2};
-    Eigen::Vector3f amb_light_intensity{10, 10, 10};
-    Eigen::Vector3f eye_pos{0, 0, 10};
-
-    float p = 150;
-
-    Eigen::Vector3f color = payload.color; 
-    Eigen::Vector3f point = payload.view_pos;
-    Eigen::Vector3f normal = payload.normal;
-
-    float kh = 0.2, kn = 0.1;
-
-    float x = normal.x();
-    float y = normal.y();
-    float z = normal.z();
-    Vector3f t = {x * y / sqrt(x * x + z * z), sqrt(x * x + z * z),
-                  z * y / sqrt(x * x + z * z)};
-    Vector3f b = normal.cross(t);
-    Matrix3f TBN;
-    TBN << t.x(), t.y(), t.z(), b.x(), b.y(), b.z(), x, y, z;
-
-    float u = payload.tex_coords.x();
-    float v = payload.tex_coords.y();
-    int width = payload.texture->width;
-    int height = payload.texture->height;
-    float dU = kh * kn * (payload.texture->getColor(u + 1.0 / width, v).norm() - payload.texture->getColor(u, v).norm());
-    float dV = kh * kn * (payload.texture->getColor(u, v + 1.0 / height).norm() - payload.texture->getColor(u, v).norm());
-    Vector3f ln = {-dU, -dV, 1};
-    normal = (TBN * ln).normalized();
-
-    Eigen::Vector3f result_color = {0, 0, 0};
-    result_color = normal;
-
-    return result_color * 255.f;
-}
-
-int main(int argc, const char** argv)
-{
-    std::vector<Triangle*> TriangleList;
-
-    float angle = 140.0;
-    bool command_line = false;
-
-    std::string filename = "v.png";
-    objl::Loader Loader;
-    std::string obj_path = "./models/spot/";
-
-    // Load .obj File
-    bool loadout = Loader.LoadFile("./models/spot/spot_triangulated_good.obj");
-    for(auto mesh:Loader.LoadedMeshes)
+    int key = -1;
+    while (key != 27) 
     {
-        for(int i=0;i<mesh.Vertices.size();i+=3)
+        for (auto &point : control_points) 
         {
-            Triangle* t = new Triangle();
-            for(int j=0;j<3;j++)
-            {
-                t->setVertex(j,Vector4f(mesh.Vertices[i+j].Position.X,mesh.Vertices[i+j].Position.Y,mesh.Vertices[i+j].Position.Z,1.0));
-                t->setNormal(j,Vector3f(mesh.Vertices[i+j].Normal.X,mesh.Vertices[i+j].Normal.Y,mesh.Vertices[i+j].Normal.Z));
-                t->setTexCoord(j,Vector2f(mesh.Vertices[i+j].TextureCoordinate.X, mesh.Vertices[i+j].TextureCoordinate.Y));
-            }
-            TriangleList.push_back(t);
-        }
-    }
-
-    rst::rasterizer r(700, 700);
-
-    auto texture_path = "hmap.jpg";
-    r.set_texture(Texture(obj_path + texture_path));
-
-    std::function<Eigen::Vector3f(fragment_shader_payload)> active_shader = phong_fragment_shader;
-
-    if (argc >= 2)
-    {
-        command_line = true;
-        filename = std::string(argv[1]);
-
-        if (argc == 3 && std::string(argv[2]) == "texture")
-        {
-            std::cout << "Rasterizing using the texture shader\n";
-            active_shader = texture_fragment_shader;
-            texture_path = "spot_texture.png";
-            r.set_texture(Texture(obj_path + texture_path));
-        }
-        else if (argc == 3 && std::string(argv[2]) == "normal")
-        {
-            std::cout << "Rasterizing using the normal shader\n";
-            active_shader = normal_fragment_shader;
-        }
-        else if (argc == 3 && std::string(argv[2]) == "phong")
-        {
-            std::cout << "Rasterizing using the phong shader\n";
-            active_shader = phong_fragment_shader;
-        }
-        else if (argc == 3 && std::string(argv[2]) == "bump")
-        {
-            std::cout << "Rasterizing using the bump shader\n";
-            active_shader = bump_fragment_shader;
-        }
-        else if (argc == 3 && std::string(argv[2]) == "displacement")
-        {
-            std::cout << "Rasterizing using the bump shader\n";
-            active_shader = displacement_fragment_shader;
-        }
-    }
-
-    Eigen::Vector3f eye_pos = {0,0,10};
-
-    r.set_vertex_shader(vertex_shader);
-    r.set_fragment_shader(active_shader);
-
-    int key = 0;
-    int frame_count = 0;
-
-    if (command_line)
-    {
-        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-        r.set_model(get_model_matrix(angle));
-        r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45.0, 1, 0.1, 50));
-
-        r.draw(TriangleList);
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
-        image.convertTo(image, CV_8UC3, 1.0f);
-        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
-
-        cv::imwrite(filename, image);
-
-        return 0;
-    }
-
-    while(key != 27)
-    {
-        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-
-        r.set_model(get_model_matrix(angle));
-        r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45.0, 1, 0.1, 50));
-
-        //r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
-        r.draw(TriangleList);
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
-        image.convertTo(image, CV_8UC3, 1.0f);
-        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
-
-        cv::imshow("image", image);
-        cv::imwrite(filename, image);
-        key = cv::waitKey(10);
-
-        if (key == 'a' )
-        {
-            angle -= 0.1;
-        }
-        else if (key == 'd')
-        {
-            angle += 0.1;
+            cv::circle(window, point, 3, {255, 255, 255}, 3);
         }
 
+        if (control_points.size() == 4) 
+        {
+            naive_bezier(control_points, window);
+            //   bezier(control_points, window);
+
+            cv::imshow("Bezier Curve", window);
+            cv::imwrite("my_bezier_curve.png", window);
+            key = cv::waitKey(0);
+
+            return 0;
+        }
+
+        cv::imshow("Bezier Curve", window);
+        key = cv::waitKey(20);
     }
-    return 0;
+
+return 0;
 }
