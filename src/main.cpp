020c73c992501c45e16456918bd5344c9bc4fbcb
Leager-zju <1004729740@qq.com>
2024-04-21 15:05:16 +0800

    assignment5 finish

--- src/main.cpp
+++ src/main.cpp
@@ -1,125 +1,39 @@
-#include <chrono>
-#include <iostream>
-#include <opencv2/opencv.hpp>
+#include "Scene.hpp"
+#include "Sphere.hpp"
+#include "Triangle.hpp"
+#include "Light.hpp"
+#include "Renderer.hpp"
 
-std::vector<cv::Point2f> control_points;
-
-void mouse_handler(int event, int x, int y, int flags, void *userdata) 
+// In the main function of the program, we create the scene (create objects and lights)
+// as well as set the options for the render (image width and height, maximum recursion
+// depth, field-of-view, etc.). We then call the render function().
+int main()
 {
-    if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4) 
-    {
-        std::cout << "Left button of the mouse is clicked - position (" << x << ", "
-        << y << ")" << '\n';
-        control_points.emplace_back(x, y);
-    }     
-}
+    Scene scene(1280, 960);
 
-void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
-{
-    auto &p_0 = points[0];
-    auto &p_1 = points[1];
-    auto &p_2 = points[2];
-    auto &p_3 = points[3];
+    auto sph1 = std::make_unique<Sphere>(Vector3f(-1, 0, -12), 2);
+    sph1->materialType = DIFFUSE_AND_GLOSSY;
+    sph1->diffuseColor = Vector3f(0.6, 0.7, 0.8);
 
-    for (double t = 0.0; t <= 1.0; t += 0.001) 
-    {
-        auto point = std::pow(1 - t, 3) * p_0 + 3 * t * std::pow(1 - t, 2) * p_1 +
-                 3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
+    auto sph2 = std::make_unique<Sphere>(Vector3f(0.5, -0.5, -8), 1.5);
+    sph2->ior = 1.5;
+    sph2->materialType = REFLECTION_AND_REFRACTION;
 
-        window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
-    }
-}
+    scene.Add(std::move(sph1));
+    scene.Add(std::move(sph2));
 
-cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
-{
-    if (control_points.size() == 1) {
-        return control_points[0];
-    }
-    std::vector<cv::Point2f> new_control_points;
-    for (int i = 0; i < control_points.size()-1; i++) {
-        new_control_points.emplace_back(t * control_points[i] + (1-t) * control_points[i+1]);
-    }
-    return recursive_bezier(new_control_points, t);
-}
+    Vector3f verts[4] = {{-5,-3,-6}, {5,-3,-6}, {5,-3,-16}, {-5,-3,-16}};
+    uint32_t vertIndex[6] = {0, 1, 3, 1, 2, 3};
+    Vector2f st[4] = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+    auto mesh = std::make_unique<MeshTriangle>(verts, vertIndex, 2, st);
+    mesh->materialType = DIFFUSE_AND_GLOSSY;
 
-void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
-{
-    for (double t = 0.0; t <= 1.0; t += 0.001)
-    {
-        cv::Point2f point = recursive_bezier(control_points, t);
-        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
-    }
-}
+    scene.Add(std::move(mesh));
+    scene.Add(std::make_unique<Light>(Vector3f(-20, 70, 20), 0.5));
+    scene.Add(std::make_unique<Light>(Vector3f(30, 50, -12), 0.5));    
 
-void bezier_antialiasing(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
-{
-    for (double t = 0.0; t <= 1.0; t += 0.001)
-    {
-        cv::Point2f point = recursive_bezier(control_points, t);
+    Renderer r;
+    r.Render(scene);
 
-        float x = point.x;
-        float y = point.y;
-        float u = x - (int)x + 0.5;
-        float v = y - (int)y + 0.5;
-        if (u > 1) {
-            u = u-1;
-        }
-        if (v > 1) {
-            v = v-1;
-        }
-
-        float d00 = pow(u, 2) + pow(v, 2);
-        float d01 = pow(1-u, 2) + pow(v, 2);
-        float d10 = pow(u, 2) + pow(1-v, 2);
-        float d11 = pow(1-u, 2) + pow(1-v, 2);
-
-        window.at<cv::Vec3b>(y, x)[1] =
-            fmin(255, window.at<cv::Vec3b>(y, x)[1] + 255 * (1 - sqrt(d00 / 2)));
-        window.at<cv::Vec3b>(y, x + 1)[1] =
-            fmin(255, window.at<cv::Vec3b>(y, x + 1)[1] + 255.0 * (1 - sqrt(d01 / 2)));
-        window.at<cv::Vec3b>(y + 1, x)[1] =
-            fmin(255, window.at<cv::Vec3b>(y + 1, x)[1] + 255.0 * (1 - sqrt(d10 / 2)));
-        window.at<cv::Vec3b>(y + 1, x + 1)[1] =
-            fmin(255, window.at<cv::Vec3b>(y + 1, x + 1)[1] + 255.0 * 1 - sqrt(d11 / 2));
-    }
-}
-
-int main() 
-{
-    cv::Mat window = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
-    cv::Mat window_antialiasing = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
-    cv::cvtColor(window, window, cv::COLOR_BGR2RGB);
-    cv::cvtColor(window_antialiasing, window_antialiasing, cv::COLOR_BGR2RGB);
-    cv::namedWindow("Bezier Curve", cv::WINDOW_AUTOSIZE);
-
-    cv::setMouseCallback("Bezier Curve", mouse_handler, nullptr);
-
-    int key = -1;
-    while (key != 27) 
-    {
-        for (auto &point : control_points) 
-        {
-            cv::circle(window, point, 3, {255, 255, 255}, 3);
-            cv::circle(window_antialiasing, point, 3, {255, 255, 255}, 3);
-        }
-
-        if (control_points.size() == 4) 
-        {
-            // naive_bezier(control_points, window);
-            bezier(control_points, window);
-            bezier_antialiasing(control_points, window_antialiasing);
-
-            cv::imshow("Bezier Curve", window);
-            cv::imwrite("my_bezier_curve.png", window);
-            cv::imwrite("my_bezier_curve_anti_aliasing.png", window_antialiasing);
-            key = cv::waitKey(0);
-
-            return 0;
-        }
-
-        cv::imshow("Bezier Curve", window);
-        key = cv::waitKey(20);
-    }
-
-return 0;
+    return 0;
 }
